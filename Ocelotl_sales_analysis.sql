5be5d930da44d8fc1ca07fadba344deb454d3060
luis Ocelotl <Luisocelot13@gmail.com>
2026-04-26 19:43:17 -0400

    setting layout to cover questions by manager

---
+++ Ocelotl_sales_analysis.sql
@@ -0,0 +1,19 @@
+-- The sales territory we will be analyzing is the NorthEast region. 
+-- Regional Director: Michael Jarvis,State:New Jersey,Sales Manager: Miami Vue
+
+/*The sales manager for your assigned territory wants to know:*/
+
+/*What is total revenue overall for sales in the assigned territory, plus the start date and end date
+that tell you what period the data covers?*/
+
+/*What is the month by month revenue breakdown for the sales territory?*/
+
+/*Provide a comparison of total revenue for the specific sales territory and the region it belongs to.*/
+
+/*What is the number of transactions per month and average transaction size by product category
+for the sales territory?*/
+
+/*Can you provide a ranking of in-store sales performance by each store in the sales territory, or a
+ranking of online sales performance by state within an online sales territory?*/
+
+/*What is your recommendation for where to focus sales attention in the next quarter?*/
